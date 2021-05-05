d3cb718db2bd2a96c21dcf7496a5b84cecf76d10
Susana Murillo <susana.murillo@uabc.edu.mx>
2021-05-05 15:16:33 -0700

    web proyect with jinja2

---
+++ jinjapy.py
@@ -0,0 +1,27 @@
+from jinja2 import FileSystemLoader, Environment
+from wsgiref.simple_server import make_server
+
+
+def app(environ, start_response):
+    #load folder
+  env = Environment(loader=FileSystemLoader("templates"))
+
+  template = env.get_template('template.html')
+  
+  #generate data
+  data = {
+      'title' : 'WSGI template render with jinja',
+      'username' : 'Takatae, dudmit'
+  }
+
+  html = template.render(data)
+  headers = [('Content-type','text/html; charset=utf-8')]
+
+  # Response code
+  start_response('200 OK', headers)
+
+  return [bytes(html,'utf-8')]
+
+with make_server('localhost',8000, app) as httpd:
+    print('Iniciando....')
+    httpd.serve_forever() 
